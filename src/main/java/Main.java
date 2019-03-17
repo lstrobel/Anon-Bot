383a6612196c2843700b5f891917b58e826eb676
Lukas Strobel <lstrobel@cs.washington.edu>
2019-03-16 21:35:16 -0700

    test commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,9 @@
+import discord4j.core.DiscordClient;
 import discord4j.core.DiscordClientBuilder;
+import discord4j.core.event.domain.lifecycle.ReadyEvent;
+import discord4j.core.event.domain.message.MessageCreateEvent;
+import discord4j.core.object.entity.Message;
+import discord4j.core.object.entity.User;
 
 import java.io.*;
 import java.util.Properties;
@@ -8,11 +13,34 @@ public class Main {
     public static void main(String[] args) {
         
         Properties properties = handleConfigFile();
-        System.out.println(properties.get("api_token"));
-        DiscordClientBuilder builder = new DiscordClientBuilder("TOKEN HERE");
+        DiscordClientBuilder builder =
+                new DiscordClientBuilder(properties.getProperty("api_token"));
+        DiscordClient client = builder.build();
+        
+        client.getEventDispatcher().on(ReadyEvent.class)
+                .subscribe(event -> {
+                    User self = event.getSelf();
+                    System.out.println(String.format("Logged in as %s#%s", self.getUsername(), self.getDiscriminator()));
+                });
+        
+        client.getEventDispatcher().on(MessageCreateEvent.class)
+                .map(MessageCreateEvent::getMessage)
+                .filter(message -> message.getAuthor().map(user -> !user.isBot()).orElse(false))
+                .filter(message -> message.getContent().orElse("").equalsIgnoreCase("!ping"))
+                .flatMap(Message::getChannel)
+                .flatMap(channel -> channel.createMessage("Pong!"))
+                .subscribe();
+        
+        client.login().block();
         
     }
     
+    /**
+     * Will read from a config.properties file in the working dir and return a corresponding
+     * Properties. If no such file exists, it will create one and then exit.
+     *
+     * @return The Properties read from config.properties
+     */
     private static Properties handleConfigFile() {
         
         Properties properties = new Properties();
